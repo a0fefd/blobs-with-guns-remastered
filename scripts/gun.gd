f21d1216c0394c94eef03eb40ab1064c0ae4310b
GutterCat42 <143992473+GutterCat42@users.noreply.github.com>
2023-09-06 18:26:23 +1000

    Add shoot sound

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -9,5 +9,6 @@ signal fired(recoil_vector)
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
 		fired.emit(-Vector2(recoil, 0).rotated(rotation))
+		$ShootSound.play()
 	
 	look_at(get_global_mouse_position())
