f5225f3dd6592c663e563c418d5035b1edb6f369
GutterCat42 <143992473+GutterCat42@users.noreply.github.com>
2023-09-06 20:23:30 +1000

    Merge get_node hotfix into main

--- scripts/bob.gd
+++ scripts/bob.gd
@@ -3,21 +3,20 @@ extends RigidBody2D
 
 @export var gun = preload("res://scenes/guns/gun.tscn")
 
-@onready var gun_pos = get_node("GunPos")
-
 var guns = []
 
 
 func add_gun(gun_to_add):
 	var gun_inst = gun_to_add.instantiate()
 	guns.append(gun_inst)
-	gun_pos.add_child(gun_inst)
+	$GunPos.add_child(gun_inst)
 	gun_inst.hide()
 	gun_inst.fired.connect(get_recoiled)
 	gun_inst.world = get_parent()
 
 
 func get_recoiled(recoil_vector):
+	#apply_impulse(recoil_vector, $GunPos.position)
 	apply_central_impulse(recoil_vector.rotated(rotation))
 
 
--- scripts/gun.gd
+++ scripts/gun.gd
@@ -6,16 +6,13 @@ extends Node2D
 
 var world
 
-@onready var animation_player = get_node("AnimationPlayer")
-@onready var shell_pos = get_node("Sprite2D/ShellPos")
-
 signal fired(recoil_vector)
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
 		fired.emit(-Vector2(recoil, 0).rotated(rotation))
-		animation_player.play("shoot")
+		$AnimationPlayer.play("shoot")
 		eject_shell()
 	
 	look_at(get_global_mouse_position())
@@ -24,4 +21,4 @@ func _process(delta):
 func eject_shell():
 	var shell_inst = shell.instantiate()
 	world.add_child(shell_inst)
-	shell_inst.global_position = shell_pos.global_position
+	shell_inst.global_position = $Sprite2D/ShellPos.global_position
