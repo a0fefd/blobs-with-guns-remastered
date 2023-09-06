d06ac1402fa6f5643449d28d8f3b9e11b9ea965f
GutterCat42 <aduqu6@eq.edu.au>
2023-09-07 09:58:39 +1000

    Add easily adjustable shell spin

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var recoil = 300
 @export var eject_force = Vector2(0, -10000)
+@export var shell_spin = 1
 @export var shell = preload("res://scenes/guns/shell.tscn")
 
 @onready var animation_player = get_node("AnimationPlayer")
@@ -26,4 +27,4 @@ func eject_shell():
 	var shell_inst = shell.instantiate()
 	world.add_child(shell_inst)
 	shell_inst.global_position = shell_pos.global_position
-	shell_inst.apply_force(eject_force, Vector2.RIGHT)
+	shell_inst.apply_force(eject_force, Vector2.RIGHT * shell_spin)
