7e544e575a740db122bfca88e78e40cd788a3ccd
GutterCat42 <aduqu6@eq.edu.au>
2023-09-07 09:56:56 +1000

    Add ejection force to shells

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 @export var recoil = 300
+@export var eject_force = Vector2(0, -10000)
 @export var shell = preload("res://scenes/guns/shell.tscn")
 
 @onready var animation_player = get_node("AnimationPlayer")
@@ -12,7 +13,7 @@ var world
 signal fired(recoil_vector)
 
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("fire"):
 		fired.emit(-Vector2(recoil, 0).rotated(rotation))
 		animation_player.play("shoot")
@@ -25,3 +26,4 @@ func eject_shell():
 	var shell_inst = shell.instantiate()
 	world.add_child(shell_inst)
 	shell_inst.global_position = shell_pos.global_position
+	shell_inst.apply_force(eject_force, Vector2.RIGHT)
