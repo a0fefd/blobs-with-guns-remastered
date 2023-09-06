c5c332c37d017b5671dfdecda6ebb29797be1c3f
GutterCat42 <143992473+GutterCat42@users.noreply.github.com>
2023-09-06 18:22:31 +1000

    Add recoil

---
+++ scripts/bob.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+
+@export var gun = preload("res://scenes/guns/gun.tscn")
+
+var guns = []
+
+
+func add_gun(gun_to_add):
+	var gun_inst = gun_to_add.instantiate()
+	guns.append(gun_inst)
+	$GunPos.add_child(gun_inst)
+	gun_inst.hide()
+	gun_inst.fired.connect(get_recoiled)
+
+
+func get_recoiled(recoil_vector):
+	#apply_impulse(recoil_vector, $GunPos.position)
+	apply_central_impulse(recoil_vector)
+
+
+func _ready():
+	add_gun(gun)
+	guns[0].show()
---
+++ scripts/gun.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+@export var recoil = 400
+
+signal fired(recoil_vector)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		fired.emit(-Vector2(recoil, 0).rotated(rotation))
+	
+	look_at(get_global_mouse_position())
