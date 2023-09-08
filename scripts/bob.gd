caabc6913a61e0e6c7436050f4b08f438e498f48
GutterCat42 <aduqu6@eq.edu.au>
2023-09-08 11:39:20 +1000

    Add player flipping

--- scripts/bob.gd
+++ scripts/bob.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 @export var gun = preload("res://scenes/guns/gun.tscn")
 
 @onready var gun_pos = get_node("GunPos")
+@onready var sprite = get_node("Sprite2D")
+@onready var collider = get_node("CollisionPolygon2D")
 
 var guns = []
 
@@ -19,9 +21,21 @@ func add_gun(gun_to_add):
 
 
 func get_recoiled(recoil_vector):
-	apply_central_impulse(recoil_vector.rotated(rotation))
+	apply_central_impulse(recoil_vector.rotated(rotation) * gun_pos.scale)
+
+
+func flip():
+	sprite.scale.x *= -1
+	collider.scale.x *= -1
+	gun_pos.position.x *= -1
+	gun_pos.scale.x *= -1
 
 
 func _ready():
 	add_gun(gun)
 	guns[0].show()
+
+
+func _process(delta):
+	if abs(guns[0].rotation_degrees) > 90:
+		flip()
