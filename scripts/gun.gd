dbc5fe9875ef433e299c68c02c7588915e708e13
GutterCat42 <143992473+GutterCat42@users.noreply.github.com>
2023-09-13 18:14:35 +1000

    Add slow motion

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -15,6 +15,7 @@ signal ammo_changed(ammo_in_mag, ammo)
 @export var shotgun := false
 @export var num_bullets: int = 10
 @export var shotgun_spread: = 40
+@export var focus_time_scale: float = 0.2
 @export var shell: PackedScene = preload("res://scenes/guns/shell.tscn")
 @export var bullet: PackedScene = preload("res://scenes/guns/bullet.tscn")
 
@@ -52,6 +53,13 @@ func _process(_delta):
 	
 	if Input.is_action_just_pressed("reload") and ammo_in_mag != mag_size and not reloading:
 		reload()
+	
+	if Input.is_action_pressed("focus"):
+		Engine.time_scale = focus_time_scale
+		AudioServer.global_rate_scale = 2 - focus_time_scale
+	else:
+		Engine.time_scale = 1
+		AudioServer.global_rate_scale = 1
 	
 	look_at(get_global_mouse_position())
 
