ce595df0ec7df29dd52ee6eadd64b2dacb00be2b
GutterCat42 <143992473+GutterCat42@users.noreply.github.com>
2023-09-25 20:54:36 +1000

    Add bullet raycasting

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -11,6 +11,7 @@ var shot_from
 var bullet_origin: Vector2
 
 @onready var world = get_parent()
+@onready var raycast = get_node("RayCast2D")
 
 
 func _ready():
@@ -24,6 +25,11 @@ func _process(delta):
 	
 	if (global_position - bullet_origin).length() > bullet_range:
 		queue_free()
+	
+	raycast.target_position = Vector2(speed * delta, 0)
+	if raycast.is_colliding():
+		global_position = raycast.get_collision_point()
+		_on_body_entered(raycast.get_collider())
 
 
 func _on_body_entered(body):
